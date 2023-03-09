b26c95c25469442e29aecd6a6465bb899ce8d865
ardisaputra2023 <ardiatwork123@gmail.com>
2023-03-09 13:39:37 +0700

    add local storage for countdown

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,53 +1,33 @@
-// local storage
-const STORAGE_KEY = "TIMER_TBFT";
+// set local storage
+function reset() {
+  localStorage.TIMER_TBFT = new Date().getTime() + 60 * 60 * 1000;
+  localStorage.DATE_TBFT = new Date().toLocaleString().split(', ')[0];
+}
 
-// Get the countdown date and time
-const countdownDate = new Date().getTime() + 60 * 60 * 1000;
+// reset local storage
+if (!localStorage.TIMER_TBFT) {
+  reset();
+}
 
 // Update the countdown every second
-const timer = setInterval(() => {
-  // Saving data as key/value pair
-  localStorage.setItem(STORAGE_KEY, [
-    hours.toString().padStart(2, "0"),
-    minutes.toString().padStart(2, "0"),
-    seconds.toString().padStart(2, "0"),
-  ]);
-
-  // // Get the data by key
-  // let name = localStorage.getItem(STORAGE_KEY);
-  // console.log("This is - ", name);
-  // let color = localStorage.getItem("color");
-  // console.log("Value of color is - ", color);
-
-  // // Get key on a given position
-  // let key1 = localStorage.key(1);
-  // console.log(key1);
-
-  // // Get number of stored items
-  // let items = localStorage.length;
-  // console.log("Total number of items is ", items);
-
-  // // Remove key with its value
-  // localStorage.removeItem("color");
-
-  // // Delete everything
-  // localStorage.clear();
-
-
-  
+const timer = setInterval(() => {  
   // Get the current date and time
   const now = new Date().getTime();
   
   // Calculate the distance between now and the countdown date
-  const distance = countdownDate - now;
+  const distance = localStorage.TIMER_TBFT - now;
   
   // Calculate hours, minutes, and seconds
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
+
   // Display the countdown in the HTML
-  document.getElementById("hours").innerHTML = hours.toString().padStart(2, "0");
-  document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, "0");
-  document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, "0");
+  if (distance >= 0 && localStorage.DATE_TBFT == new Date().toLocaleString().split(', ')[0]) {
+    document.getElementById("hours").innerHTML = hours.toString().padStart(2, "0");
+    document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, "0");
+    document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, "0");
+  } else {
+    reset();
+  }
 }, 1000);
